ff3e018b7ef501269f79fd0e667ad500d132fae3
Lanre <agbenla.lanre@gmail.com>
2017-10-31 11:27:28 +0100

    Change Port Config

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,5 @@
 module.exports = {
-// Development configuration options
+// Production configuration options
 db: 'mongodb://hercoolhis:larrydon007@ds121535.mlab.com:21535/andela_students',
 db2: 'mongodb://localhost:27017/students'
 };
--- server.js
+++ server.js
@@ -4,5 +4,7 @@ var express = require('./config/express');
 var db = mongoose();
 var app = express();
 
+//app.listen(process.env.NODE_ENV);
+
 module.exports = app;
 console.log('Server running at http://localhost:3000/');
